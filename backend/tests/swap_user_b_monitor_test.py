440772bd17d3ce4201981f0f9f19893211eb1789
njuyuanrui <njuyuanrui@163.com>
2020-05-19 10:55:28 +0800

    update copywrite

--- backend/tests/swap_user_b_monitor_test.py
+++ backend/tests/swap_user_b_monitor_test.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# Copyright 2019 TEEX
+# Copyright 2020 Shanghai Gejing InfoTech Co.,Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
